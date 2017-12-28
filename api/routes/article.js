38fdeedbc86e6aa679354ecef85e5dac23e181d5
RedPi <pierre.lerouge.dev@gmail.com>
2017-12-28 12:47:33 +0100

    eslintrc and respect ES6

--- api/routes/article.js
+++ api/routes/article.js
@@ -1,30 +1,28 @@
-var express = require('express');
-var config = require('../../config/database');
-var mongoose = require('mongoose');
+const express = require('express');
+const config = require('../../config/database');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-var db = mongoose.connection.openUri(config.db);
-var articlesCollection = db.collection("articles");
-var articlesRoutes = express.Router();
+const db = mongoose.connection.openUri(config.db);
+const articlesCollection = db.collection('articles');
+const articlesRoutes = express.Router();
 
 articlesRoutes.route('/articles')
-    // Extract and save metadata from RSS feed
-    .post(function (req, res) {
-        var articles = req.body;
-        for (var i = 0, len = articles.length; i < len; i++) {
-            const item = articles[i];
-            articlesCollection.find({guid:item.guid}).toArray(function (err, docs) {
+// Extract and save metadata from RSS feed
+    .post((req, res) => {
+        const articles = req.body;
+        for (const article of articles) {
+            articlesCollection.find({ guid: article.guid }).toArray((err, docs) => {
                 if (docs.length === 0) {
-                    articlesCollection.insertOne(item, function (err) {
-                        if (err) {
+                    articlesCollection.insertOne(article, (error) => {
+                        if (error) {
                             console.log('Unable to save item');
                         }
                     });
                 }
             });
-
         }
 
-        return res.json({'status': 200, 'msg': ' Articles added successfully'});
+        return res.json({ status: 200, msg: ' Articles added successfully' });
     });
 
 module.exports = articlesRoutes;
